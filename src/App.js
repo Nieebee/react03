523adcff80d7302ffc24e9f2c02cc17bbce7019b
Niédja Bezerra <niedjacarlos@gmail.com>
2022-02-14 00:08:49 -0300

    prática 3 - react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    name: "Niedja",
+    age: 29,
+    favFood: "Strogonoff",
+    favSongs: [
+      "Easy on Me - Adele",
+      "Rewrite the Stars - The Greatest Showman",
+      "Helena - My Chemical Romance",
+      "Umbrella - Rihanna",
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <h1>Meu nome é {this.state.name}</h1>
+        <h2>Eu tenho {this.state.age}</h2>
+        <h3>Minha comida favorita é {this.state.favFood}</h3>
+        <ul>
+          <p>Minhas músicas favoritas são:</p>
+          {this.state.favSongs.map((songs, id) => (
+            <li key={id}>{songs}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
